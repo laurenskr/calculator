7ecbe41f7fd54dbde2a25f93e2ef31f66d519eb2
Laurens Krom <laurens.krom@gmail.com>
2020-04-16 22:53:51 +0200

    started on keydown listening

--- calculator.js
+++ calculator.js
@@ -67,6 +67,17 @@ operators.forEach(button => button.addEventListener("click", () => {
 
 }
 ));
+document.addEventListener('keydown', function(event) {
+    if ( (event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 || event.keyCode <= 105)){
+        displayContents.innerText += String.fromCharCode(event.keyCode)
+    }
+    if(event.keyCode == 37) {
+        alert('Left was pressed');
+    }
+    else if(event.keyCode == 39) {
+        alert('Right was pressed');
+    }
+});
 equals.addEventListener("click", () =>{
     if (prev){
         curr = displayContents.innerText;
